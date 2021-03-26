ededfee30aa827902bc8d384c75b2a7845f4235b
GitHub Action <action@github.com>
2021-03-26 13:01:18 +0000

    Add generated data

--- docs/github_action_report.js
+++ docs/github_action_report.js
@@ -58,11 +58,27 @@ function drawChart() {
         ['Failure', 608]
     ]);
     var options = {
-        title: 'Runs Overview',
+        title: 'Runs Overview for the last 5 runs',
         slices: [{ color: 'green' }, { color: 'red' }],
         pieHole: 0.4,
     };
-    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart_total_runs'));
     chart.draw(data, options);
 }
 google.charts.setOnLoadCallback(drawChart);
+
+function drawChart_last_run() {
+    var data = google.visualization.arrayToDataTable([
+        ['Job Status', 'Total'],
+        ['Success', 601],
+        ['Failure', 11]
+    ]);
+    var options = {
+        title: 'Runs Overview for last night run',
+        slices: [{ color: 'green' }, { color: 'red' }],
+        pieHole: 0.4,
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart_last_run'));
+    chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(drawChart_last_run);
